a01df0825867649722fd64a60b99b3a651ee0b90
spheal363 <115055083+spheal363@users.noreply.github.com>
2025-04-09 20:27:39 +0900

    feature: cinemachineのカメラを設定

---
+++ Assets/Scripts/RoguelikeGamePlayer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class RoguelikeGamePlayer : BasePlayer {
+    public static RoguelikeGamePlayer Instance { get; private set; }
+
+    private Transform playerTransform;
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private GameInputManager gameInputManager;
+
+    private Vector2 inputVector;
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this;
+    }
+
+    private void Update() {
+        // ÉvÉåÉCÉÑÅ[ÇÃà⁄ìÆÇêßå‰Ç∑ÇÈ
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        inputVector = gameInputManager.GetMovementVectorNormalized();
+        Vector3 moveDirection = new Vector3(inputVector.x, inputVector.y, 0f);
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        this.transform.position += moveDirection * moveDistance;
+
+        isMoving = inputVector != Vector2.zero;
+    }
+}
